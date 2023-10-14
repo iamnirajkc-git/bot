4ea6e78d2ee2a2eb9a7d4b95bf8bf74b81760f39
Niraj <kcniraj44@gmail.com>
2023-10-13 19:38:16 -0500

    updated requirements.txt

--- gunicorn_config.py
+++
@@ -1,3 +0,0 @@
-workers = 2  # The number of worker processes
-bind = "127.0.0.1:8000"  # The host and port where your app should be served
-module = "app:app"  # Replace "app" with the name of your Flask application object
